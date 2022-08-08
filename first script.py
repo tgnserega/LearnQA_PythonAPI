189430bf87679db2d41a4095743bc2c2c0ceb185
Sergey Pashkov <tgnserega@gmail.com>
2022-08-08 21:28:29 +0300

    Ex14: Формирование фреймворка

--- first script.py
+++
@@ -1,5 +0,0 @@
-import requests
-
-payload = {"name": "User"}
-response = requests.get("https://playground.learnqa.ru/api/get_text", params=payload)
-print(response.text)
--- json_parsing.py
+++
@@ -1,8 +0,0 @@
-import json
-
-json_text = '{"messages":[{"message":"This is the first message","timestamp":"2021-06-04 16:40:53"},{"message":"And this is a second message","timestamp":"2021-06-04 16:41:01"}]}'
-obj = json.loads(json_text)
-obj1 = obj['messages']
-obj2 = obj1[1]
-
-print(obj2['message'])
