ae2a14edf729e8fb526dd296203d88f75f10a4a0
Sergey Pashkov <tgnserega@gmail.com>
2022-08-08 18:13:04 +0300

    Ex11: Тест запроса на метод cookie

---
+++ test_home_cookie.py
@@ -0,0 +1,11 @@
+import requests
+
+class TestHomeCookie:
+
+    def test_home_cookies(self):
+        response = requests.get("https://playground.learnqa.ru/api/homework_cookie")
+        cookie = dict(response.cookies)
+        print(cookie)
+        assert response.status_code == 200, "Wrong response status"
+        assert 'HomeWork' in cookie, "There is now homework cookie in the response"
+        assert cookie.get("HomeWork") == 'hw_value', "The cookie value does not match hw_value"
